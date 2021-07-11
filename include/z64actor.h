50e09918de27f6b3239429c374051398a06ffbf4
turpaan64 <65511061+turpaan64@users.noreply.github.com>
2021-07-11 22:48:20 +0300

    All set

--- include/z64actor.h
+++ include/z64actor.h
@@ -156,7 +156,9 @@ typedef struct Actor {
     /* 0x130 */ ActorFunc update; // Update Routine. Called by `Actor_UpdateAll`
     /* 0x134 */ ActorFunc draw; // Draw Routine. Called by `Actor_Draw`
     /* 0x138 */ ActorOverlay* overlayEntry; // Pointer to the overlay table entry for this actor
+    #ifndef _Z64OVL_10_
     /* 0x13C */ char dbgPad[0x10]; // Padding that only exists in the debug rom
+    #endif
 } Actor; // size = 0x14C
 
 typedef enum {
--- oot_debug.h
+++ oot_debug.h
@@ -2,6 +2,6 @@
 #define _Z64HDR_OOT_DEBUG_H_
 #define _Z64OVL_DEBUG_
 
-#include "z64hdr.h"
+#include "include/z64.h"
 
 #endif
