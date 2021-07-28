8a0168343917b5df6e6e8bb3ac68f7e0f82fb4d5
Sauraen <sauraen@gmail.com>
2021-07-27 22:56:10 -0700

    Added sqrtf, sqrt, and some fabsf symbols

--- intrinsics.h
+++ intrinsics.h
@@ -2,7 +2,7 @@
 #error "This file must be included from functions.h"
 #endif
 
-#if defined(_Z64HDR_10_) || defined(_Z64HDR_USE_INTRINSICS_)
+#ifdef _Z64HDR_USE_INTRINSICS_
 #define fabsf __builtin_fabsf
 #define sqrtf __builtin_sqrtf
 #define sqrt __builtin_sqrt
@@ -10,7 +10,21 @@
 f32 fabsf(f32 f);
 f32 sqrtf(f32 f);
 f64 sqrt(f64 d);
+
+#if defined(_Z64HDR_10_) && !defined(_Z64HDR_DISABLE_FABSF_ASM_)
+//1.0 has implementations for sqrtf and sqrt (double), but no abs / fabsf / etc.
+//You can either use intrinsics or this inline asm. However, unless this is
+//disabled, it'll get emitted into every file you compile. (It's only two
+//instructions though.)
+asm(
+    "fabsf:\n"
+    "jr    $ra \n"
+    "abs.s $f0, $f12\n"
+);
 #endif
+
+#endif
+
 
 /*
 //These are Microsoft intrinsics, possibly also IDO, but not GCC compatible
