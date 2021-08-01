6af4768f641bb3f8af98c8cccba8a9fb59f05765
turpaan64 <65511061+turpaan64@users.noreply.github.com>
2021-08-01 18:50:55 +0300

    Small oversights

--- helpers/Physics.h
+++ helpers/Physics.h
@@ -79,19 +79,19 @@ typedef struct {
 } PhysicsStrand;
 
 _Z64HDR_HELPER_PREFIX_
-void Physics_GetHeadProperties(PhysicsStrand* params, Vec3f* mult, s32 flag) {
+void Physics_GetHeadProperties(PhysicsStrand* strand, Vec3f* mult, s32 flag) {
 	static MtxF mtxF;
 	Vec3f zero = { 0 };
 	
 	Matrix_Get(&mtxF);
-	params->head.mtxF = &mtxF;
-	func_800D20CC(&mtxF, &params->head.rot, flag);
+	strand->head.mtxF = &mtxF;
+	func_800D20CC(&mtxF, &strand->head.rot, flag);
 	if (mult == NULL) {
-		Matrix_MultVec3f(&zero, &params->head.pos);
+		Matrix_MultVec3f(&zero, &strand->head.pos);
 		
 		return;
 	}
-	Matrix_MultVec3f(mult, &params->head.pos);
+	Matrix_MultVec3f(mult, &strand->head.pos);
 }
 
 _Z64HDR_HELPER_PREFIX_
@@ -111,7 +111,7 @@ void Physics_SetPhysicsStrand(PhysicsStrandInit* init, PhysicsStrand* dest, f32*
 }
 
 _Z64HDR_HELPER_PREFIX_
-void Physics_DrawDynamicStrand(GraphicsContext* gfxCtx, TwoHeadGfxArena* disp, PhysicsJoint* jointTable, PhysicsStrand* param, void* callback, void* callbackArg1, void* callbackArg2) {
+void Physics_DrawDynamicStrand(GraphicsContext* gfxCtx, TwoHeadGfxArena* disp, PhysicsJoint* jointTable, PhysicsStrand* strand, void* callback, void* callbackArg1, void* callbackArg2) {
 	s32 i;
 	f32 tempY;
 	f32 angX;
@@ -120,7 +120,7 @@ void Physics_DrawDynamicStrand(GraphicsContext* gfxCtx, TwoHeadGfxArena* disp, P
 	Vec3f posAdd;
 	Vec3f rigidity, smoothedRigid;
 	Vec3f velAdj = { 0 };
-	s16 headRotY = param->head.rot.y;
+	s16 headRotY = strand->head.rot.y;
 	Vec3f* pPos;
 	Vec3f* pRot;
 	Vec3f* pVel;
@@ -129,26 +129,26 @@ void Physics_DrawDynamicStrand(GraphicsContext* gfxCtx, TwoHeadGfxArena* disp, P
 	
 	Matrix_Push();
 	
-	jointTable[0].pos.x = param->head.pos.x;
-	jointTable[0].pos.y = param->head.pos.y;
-	jointTable[0].pos.z = param->head.pos.z;
+	jointTable[0].pos.x = strand->head.pos.x;
+	jointTable[0].pos.y = strand->head.pos.y;
+	jointTable[0].pos.z = strand->head.pos.z;
 	
-	for (i = 1; i < param->info.numLimbs + 1; i++) {
-		Math_ApproachF(&jointTable[i].vel.x, 0.0f, 1.0f, param->info.velStep);
-		Math_ApproachF(&jointTable[i].vel.y, 0.0f, 1.0f, param->info.velStep);
-		Math_ApproachF(&jointTable[i].vel.z, 0.0f, 1.0f, param->info.velStep);
+	for (i = 1; i < strand->info.numLimbs + 1; i++) {
+		Math_ApproachF(&jointTable[i].vel.x, 0.0f, 1.0f, strand->info.velStep);
+		Math_ApproachF(&jointTable[i].vel.y, 0.0f, 1.0f, strand->info.velStep);
+		Math_ApproachF(&jointTable[i].vel.z, 0.0f, 1.0f, strand->info.velStep);
 	}
 	
-	Matrix_RotateX_s(param->head.rot.x, MTXMODE_NEW);
-	Matrix_RotateY_s(param->head.rot.y, MTXMODE_APPLY);
-	Matrix_RotateZ_s(param->head.rot.z, MTXMODE_APPLY);
-	if (param->rigidity.rot.x) Matrix_RotateX_f(param->rigidity.rot.x, MTXMODE_APPLY);
-	if (param->rigidity.rot.y) Matrix_RotateY_f(param->rigidity.rot.y, MTXMODE_APPLY);
-	if (param->rigidity.rot.z) Matrix_RotateZ_f(param->rigidity.rot.z, MTXMODE_APPLY);
-	Matrix_MultVec3f(&param->rigidity.push, &rigidity);
+	Matrix_RotateX_s(strand->head.rot.x, MTXMODE_NEW);
+	Matrix_RotateY_s(strand->head.rot.y, MTXMODE_APPLY);
+	Matrix_RotateZ_s(strand->head.rot.z, MTXMODE_APPLY);
+	if (strand->rigidity.rot.x) Matrix_RotateX_f(strand->rigidity.rot.x, MTXMODE_APPLY);
+	if (strand->rigidity.rot.y) Matrix_RotateY_f(strand->rigidity.rot.y, MTXMODE_APPLY);
+	if (strand->rigidity.rot.z) Matrix_RotateZ_f(strand->rigidity.rot.z, MTXMODE_APPLY);
+	Matrix_MultVec3f(&strand->rigidity.push, &rigidity);
 	
 	// Main calculation loop
-	for (i = 1; i < param->info.numLimbs + 1; i++) {
+	for (i = 1; i < strand->info.numLimbs + 1; i++) {
 		pPos = &jointTable[i].pos;
 		pVel = &jointTable[i].vel;
 		pPrevPos = &jointTable[i - 1].pos;
@@ -156,22 +156,22 @@ void Physics_DrawDynamicStrand(GraphicsContext* gfxCtx, TwoHeadGfxArena* disp, P
 		
 		// Smoothens curve at the start of the limb array
 		smoothedRigid = (Vec3f) { 0 };
-		if (i < param->rigidity.num) {
-			smoothedRigid.x = ((param->rigidity.num - i) * rigidity.x) * param->rigidity.mult;
-			smoothedRigid.y = ((param->rigidity.num - i) * rigidity.y) * param->rigidity.mult;
-			smoothedRigid.z = ((param->rigidity.num - i) * rigidity.z) * param->rigidity.mult;
+		if (i < strand->rigidity.num) {
+			smoothedRigid.x = ((strand->rigidity.num - i) * rigidity.x) * strand->rigidity.mult;
+			smoothedRigid.y = ((strand->rigidity.num - i) * rigidity.y) * strand->rigidity.mult;
+			smoothedRigid.z = ((strand->rigidity.num - i) * rigidity.z) * strand->rigidity.mult;
 		}
 		
 		workVec.x = pPos->x + pVel->x - pPrevPos->x + smoothedRigid.x;
-		tempY = pPos->y + pVel->y + param->info.gravity + smoothedRigid.y;
+		tempY = pPos->y + pVel->y + strand->info.gravity + smoothedRigid.y;
 		workVec.z = pPos->z + pVel->z - pPrevPos->z + smoothedRigid.z;
 		
 		// FLOOR, also gets rid of the smoothedRigid
-		if (tempY < param->info.floorY + 10.0f) {
+		if (tempY < strand->info.floorY + 10.0f) {
 			workVec.x -= smoothedRigid.x;
 			workVec.z -= smoothedRigid.z;
-			if (i != param->info.numLimbs + 1 && tempY < param->info.floorY)
-				tempY = CLAMP_MIN(tempY, param->info.floorY);
+			if (i != strand->info.numLimbs + 1 && tempY < strand->info.floorY)
+				tempY = CLAMP_MIN(tempY, strand->info.floorY);
 		}
 		
 		workVec.y = tempY - pPrevPos->y;
@@ -182,44 +182,44 @@ void Physics_DrawDynamicStrand(GraphicsContext* gfxCtx, TwoHeadGfxArena* disp, P
 		pPrevRot->x = angX;
 		
 		// Handle constraints if they are set
-		if (param->constraint.rotStepCalc.x || param->constraint.rotStepCalc.y || param->constraint.lockRoot) {
+		if (strand->constraint.rotStepCalc.x || strand->constraint.rotStepCalc.y || strand->constraint.lockRoot) {
 			s16 rootAngleX, rootAngleY;
 			s16 workAngleX, workAngleY;
 			s16 tempAngleX, tempAngleY;
 			
 			// Handle root rotations differently
 			if (i == 1) {
-				if (param->constraint.lockRoot) {
+				if (strand->constraint.lockRoot) {
 					pPrevRot->x = angX = -Math_Atan2F(sqrtf(SQ(rigidity.x) + SQ(rigidity.z)), rigidity.y);
 					pPrevRot->y = angY = Math_Atan2F(rigidity.z, rigidity.x);
 				} else {
-					if (param->constraint.rotStepCalc.x) {
+					if (strand->constraint.rotStepCalc.x) {
 						rootAngleX = RADF_TO_BINANG(-Math_Atan2F(sqrtf(SQ(rigidity.x) + SQ(rigidity.z)), rigidity.y));
 						workAngleX = RADF_TO_BINANG(-Math_Atan2F(sqrtf(SQ(workVec.x) + SQ(workVec.z)), workVec.y));
-						Math_SmoothStepToS(&rootAngleX, workAngleX, 1, DEGF_TO_BINANG(param->constraint.rotStepCalc.x), 1);
+						Math_SmoothStepToS(&rootAngleX, workAngleX, 1, DEGF_TO_BINANG(strand->constraint.rotStepCalc.x), 1);
 						angX = BINANG_TO_RAD(rootAngleX);
 						pPrevRot->x = angX;
 					}
-					if (param->constraint.rotStepCalc.y) {
+					if (strand->constraint.rotStepCalc.y) {
 						rootAngleY = RADF_TO_BINANG(Math_Atan2F(rigidity.z, rigidity.x));
 						workAngleY = RADF_TO_BINANG(Math_Atan2F(workVec.z, workVec.x));
-						Math_SmoothStepToS(&rootAngleY, workAngleY, 1, DEGF_TO_BINANG(param->constraint.rotStepCalc.y), 1);
+						Math_SmoothStepToS(&rootAngleY, workAngleY, 1, DEGF_TO_BINANG(strand->constraint.rotStepCalc.y), 1);
 						angY = BINANG_TO_RAD(rootAngleY);
 						pPrevRot->y = angY;
 					}
 				}
 			} else {
-				if (param->constraint.rotStepCalc.x) {
+				if (strand->constraint.rotStepCalc.x) {
 					tempAngleX = RADF_TO_BINANG(jointTable[i - 2].rot.x);
 					workAngleX = RADF_TO_BINANG(-Math_Atan2F(sqrtf(SQ(workVec.x) + SQ(workVec.z)), workVec.y));
-					Math_SmoothStepToS(&tempAngleX, workAngleX, 1, DEGF_TO_BINANG(param->constraint.rotStepCalc.x), 1);
+					Math_SmoothStepToS(&tempAngleX, workAngleX, 1, DEGF_TO_BINANG(strand->constraint.rotStepCalc.x), 1);
 					angX = BINANG_TO_RAD(tempAngleX);
 					pPrevRot->x = angX;
 				}
-				if (param->constraint.rotStepCalc.y) {
+				if (strand->constraint.rotStepCalc.y) {
 					tempAngleY = RADF_TO_BINANG(jointTable[i - 2].rot.y);
 					workAngleY = RADF_TO_BINANG(Math_Atan2F(workVec.z, workVec.x));
-					Math_SmoothStepToS(&tempAngleY, workAngleY, 1, DEGF_TO_BINANG(param->constraint.rotStepCalc.y), 1);
+					Math_SmoothStepToS(&tempAngleY, workAngleY, 1, DEGF_TO_BINANG(strand->constraint.rotStepCalc.y), 1);
 					angY = BINANG_TO_RAD(tempAngleY);
 					pPrevRot->y = angY;
 				}
@@ -228,24 +228,24 @@ void Physics_DrawDynamicStrand(GraphicsContext* gfxCtx, TwoHeadGfxArena* disp, P
 		
 		Matrix_RotateY(angY, MTXMODE_NEW);
 		Matrix_RotateX(angX, MTXMODE_APPLY);
-		Matrix_MultZ(ABS(param->limbsLength[i]) * param->gfx.scale.z, &posAdd);
+		Matrix_MultZ(ABS(strand->limbsLength[i]) * strand->gfx.scale.z, &posAdd);
 		
 		// Pushes limbs away from selected Vec3f points
-		for (s32 i = 0; i < param->spheres.num; i++) {
+		for (s32 i = 0; i < strand->spheres.num; i++) {
 			Vec3f tempPosAdd = {
 				pPrevPos->x + posAdd.x,
 				pPrevPos->y + posAdd.y,
 				pPrevPos->z + posAdd.z
 			};
-			f32 radiusTo = Math_Vec3f_DistXYZ(&tempPosAdd, &param->spheres.centers[i]);
+			f32 radiusTo = Math_Vec3f_DistXYZ(&tempPosAdd, &strand->spheres.centers[i]);
 			
-			if (radiusTo < param->spheres.radius) {
-				s16 yaw = Math_Vec3f_Yaw(&param->spheres.centers[i], &tempPosAdd);
-				s16 pitch = Math_Vec3f_Pitch(&param->spheres.centers[i], &tempPosAdd);
-				posAdd.x += Math_SinS(headRotY + 0x8000) * (param->spheres.radius - radiusTo) * 0.5f;
-				posAdd.z += Math_CosS(headRotY + 0x8000) * (param->spheres.radius - radiusTo) * 0.5f;
-				velAdj.x += Math_SinS(headRotY + 0x8000) * (param->spheres.radius - radiusTo) * 0.5f;
-				velAdj.z += Math_CosS(headRotY + 0x8000) * (param->spheres.radius - radiusTo) * 0.5f;
+			if (radiusTo < strand->spheres.radius) {
+				s16 yaw = Math_Vec3f_Yaw(&strand->spheres.centers[i], &tempPosAdd);
+				s16 pitch = Math_Vec3f_Pitch(&strand->spheres.centers[i], &tempPosAdd);
+				posAdd.x += Math_SinS(headRotY + 0x8000) * (strand->spheres.radius - radiusTo) * 0.5f;
+				posAdd.z += Math_CosS(headRotY + 0x8000) * (strand->spheres.radius - radiusTo) * 0.5f;
+				velAdj.x += Math_SinS(headRotY + 0x8000) * (strand->spheres.radius - radiusTo) * 0.5f;
+				velAdj.z += Math_CosS(headRotY + 0x8000) * (strand->spheres.radius - radiusTo) * 0.5f;
 				
 				pPrevRot->y = angY =  Math_Atan2F(posAdd.z, posAdd.x);
 				pPrevRot->x = angX = -Math_Atan2F(sqrtf(SQ(posAdd.x) + SQ(posAdd.z)), posAdd.y);
@@ -260,51 +260,51 @@ void Physics_DrawDynamicStrand(GraphicsContext* gfxCtx, TwoHeadGfxArena* disp, P
 		pPos->y = pPrevPos->y + posAdd.y;
 		pPos->z = pPrevPos->z + posAdd.z;
 		
-		pVel->x = (pPos->x - workVec.x + velAdj.x) * param->info.velMult;
-		pVel->y = (pPos->y - workVec.y + velAdj.y) * param->info.velMult;
-		pVel->z = (pPos->z - workVec.z + velAdj.z) * param->info.velMult;
+		pVel->x = (pPos->x - workVec.x + velAdj.x) * strand->info.velMult;
+		pVel->y = (pPos->y - workVec.y + velAdj.y) * strand->info.velMult;
+		pVel->z = (pPos->z - workVec.z + velAdj.z) * strand->info.velMult;
 		
 		jointTable[i].vel = (Vec3f) {
-			CLAMP(pVel->x, -param->info.maxVel, param->info.maxVel),
-			CLAMP(pVel->y, -param->info.maxVel, param->info.maxVel),
-			CLAMP(pVel->z, -param->info.maxVel, param->info.maxVel),
+			CLAMP(pVel->x, -strand->info.maxVel, strand->info.maxVel),
+			CLAMP(pVel->y, -strand->info.maxVel, strand->info.maxVel),
+			CLAMP(pVel->z, -strand->info.maxVel, strand->info.maxVel),
 		};
 	}
 	
-	if (param->gfx.noDraw) {
+	if (strand->gfx.noDraw) {
 		Matrix_Pull();
 		
 		return;
 	}
 	
-	Mtx* matrix = Graph_Alloc(gfxCtx, param->info.numLimbs * sizeof(Mtx));
+	Mtx* matrix = Graph_Alloc(gfxCtx, strand->info.numLimbs * sizeof(Mtx));
 	s16 y = RADF_TO_BINANG(jointTable[0].rot.y);
 	s16 x = RADF_TO_BINANG(jointTable[0].rot.x);
 	
-	for (i = 0; i < param->info.numLimbs; i++) {
+	for (i = 0; i < strand->info.numLimbs; i++) {
 		pPos = &jointTable[i].pos;
 		pRot = &jointTable[i].rot;
 		
 		Matrix_Translate(pPos->x, pPos->y, pPos->z, MTXMODE_NEW);
 		
-		if (param->constraint.rotStepDraw.y) {
-			Math_SmoothStepToS(&y, RADF_TO_BINANG(pRot->y), 3, DEGF_TO_BINANG(param->constraint.rotStepDraw.y), 1);
-			Matrix_RotateY(BINANG_TO_RAD(y), MTXMODE_APPLY);
+		if (strand->constraint.rotStepDraw.y) {
+			Math_SmoothStepToS(&y, RADF_TO_BINANG(pRot->y), 3, DEGF_TO_BINANG(strand->constraint.rotStepDraw.y), 1);
+			Matrix_RotateY_s(y, MTXMODE_APPLY);
 		} else {
 			Matrix_RotateY(pRot->y, MTXMODE_APPLY);
 		}
 		
-		if (param->constraint.rotStepDraw.x) {
-			Math_SmoothStepToS(&x, RADF_TO_BINANG(pRot->x), 3, DEGF_TO_BINANG(param->constraint.rotStepDraw.x), 1);
-			Matrix_RotateX(BINANG_TO_RAD(x), MTXMODE_APPLY);
+		if (strand->constraint.rotStepDraw.x) {
+			Math_SmoothStepToS(&x, RADF_TO_BINANG(pRot->x), 3, DEGF_TO_BINANG(strand->constraint.rotStepDraw.x), 1);
+			Matrix_RotateX_s(x, MTXMODE_APPLY);
 		} else {
 			Matrix_RotateX(pRot->x, MTXMODE_APPLY);
 		}
 		
-		if (param->limbsLength[i] < 0)
-			Matrix_Scale(param->gfx.scale.x, param->gfx.scale.y, -param->gfx.scale.z, MTXMODE_APPLY);
+		if (strand->limbsLength[i] < 0)
+			Matrix_Scale(-strand->gfx.scale.x, strand->gfx.scale.y, -strand->gfx.scale.z, MTXMODE_APPLY);
 		else
-			Matrix_Scale(param->gfx.scale.x, param->gfx.scale.y, param->gfx.scale.z, MTXMODE_APPLY);
+			Matrix_Scale(strand->gfx.scale.x, strand->gfx.scale.y, strand->gfx.scale.z, MTXMODE_APPLY);
 		
 		if(callback) {
 			((PhysicCallback)callback)(i, callbackArg1, callbackArg2);
@@ -313,8 +313,8 @@ void Physics_DrawDynamicStrand(GraphicsContext* gfxCtx, TwoHeadGfxArena* disp, P
 	}
 	
 	gDPPipeSync(disp->p++);
-	gSPSegment(disp->p++, param->gfx.segID, matrix);
-	gSPDisplayList(disp->p++, param->gfx.dlist);
+	gSPSegment(disp->p++, strand->gfx.segID, matrix);
+	gSPDisplayList(disp->p++, strand->gfx.dlist);
 	Matrix_Pull();
 }
 
