434c30c979576fb82c494a89a5db774fa09027b8
rankaisija64 <65511061+rankaisija64@users.noreply.github.com>
2022-07-03 19:47:34 +0300

    Just define this

--- include/global.h
+++ include/global.h
@@ -1,6 +1,8 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
+#define _Z64HDR_OOT_MQ_DEBUG_H_
+
 #include "functions.h"
 #include "variables.h"
 #include "macros.h"
--- z64hdr.h
+++ z64hdr.h
@@ -1,4 +1,5 @@
 #ifndef _Z64HDR_H_
+#define _Z64HDR_H_
 
 #include "common/assert_version_set.h"
 
