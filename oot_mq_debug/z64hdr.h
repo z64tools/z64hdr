dc6a860a132046822fee712d0107f6f0ed5aa92f
rankaisija64 <65511061+rankaisija64@users.noreply.github.com>
2022-09-01 22:06:47 +0300

    Another Update

--- oot_mq_debug/z64hdr.h
+++ oot_mq_debug/z64hdr.h
@@ -1,6 +1,11 @@
 #ifndef Z64HDR_H
 #define Z64HDR_H
 
+#ifndef GLOBAL_H
+  #include "ultra64.h"
+  #include "global.h"
+#endif
+
 #define OOT_MQ_DEBUG_PAL
 
 extern struct GraphicsContext* __gfxCtx;
--- oot_u10/z64hdr.h
+++ oot_u10/z64hdr.h
@@ -1,6 +1,11 @@
 #ifndef Z64HDR_H
 #define Z64HDR_H
 
+#ifndef GLOBAL_H
+  #include "ultra64.h"
+  #include "global.h"
+#endif
+
 #define OOT_U10
 
 extern struct GraphicsContext* __gfxCtx;
